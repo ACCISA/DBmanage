dfb817624e9fc310dca9979fc1243c44e1b49ea5
Romullus <39713625+ACCISA@users.noreply.github.com>
2023-05-09 16:45:22 -0400

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import IndexPage from "./pages/IndexPage";
+import DashboardPage from "./pages/DashboardPage"
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+
+function App() {
+  const [count, setCount] = useState(0);
+  axios.defaults.baseURL = "http://localhost:4000";
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Navbar, Button } from "flowbite-react";
+
+export default function Header() {
+  return (
+    <Navbar fluid={true} rounded={true}>
+      <Navbar.Brand href="/" className="gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+          />
+        </svg>
+
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          DB Manage
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="/navbars" active={true}>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">About</Navbar.Link>
+        <Navbar.Link href="/navbars">Services</Navbar.Link>
+        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+        <Navbar.Link href="/navbars">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
