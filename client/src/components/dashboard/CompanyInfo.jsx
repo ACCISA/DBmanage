183e0c045dde3de38cb1cc6e7de51e230ec5da4c
Romullus <39713625+ACCISA@users.noreply.github.com>
2023-05-17 18:17:35 -0400

    component edit

--- client/src/components/dashboard/CompanyInfo.jsx
+++ client/src/components/dashboard/CompanyInfo.jsx
@@ -16,12 +16,16 @@ export default function CompanyInfo() {
       <h1 className="font-bold text-2xl m-4 capitalize">{profile.company}</h1>
       <div>{profile.company}</div>
       <div>Latest News</div>
+
+      <div className="border p-4 flex flex-col justify-center items-center m-4">
       <div>Database</div>
-      <div className="gid grid-flow-row col-span-4">
+        <div className="flex flex-row row-span-4">
         <Button className="m-4">Database 1</Button>
         <Button className="m-4">Database 2</Button>
         <Button className="m-4">Database 3</Button>
         <Button className="m-4">Database 4</Button>
+        </div>
+        
       </div>
     </div>
   );
